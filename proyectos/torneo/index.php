687bc54f602dbbe4fffe66e758b90d7f1d5fe3e8
IsaiasLeonel00012 <isaiasleonel2004@gmail.com>
2026-09-14 22:03:23 -0600

    agregando html

---
+++ proyectos/torneo/index.php
@@ -0,0 +1,96 @@
+<?php 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+   $nombre = $_POST["nombre"];
+   $edad = $_POST["edad"];
+   $correo = $_POST["correo"];
+   $videojuego = $_POST["videojuego"];
+   $modalidad = $_POST["modalidad"];
+   $experiencia = $_POST["experencia"];
+
+
+   echo "nombre". $nombre. "<br>";
+   echo "edad". $edad."<br>";
+   echo "correo" . $correo."<br>";
+   echo "videojuego". $videojuego."<br>";
+   echo "modalidad" . $modalidad."<br>";
+   echo "experencia". $experiencia."<br>";
+
+
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Incripciones al Torneo</h1>
+
+    <form method="POST">
+        <label for="text">Nombre del participante</label>
+        <input type="text" name="nombre">
+
+        <br><br>
+
+        <label for="text">Edad</label>
+        <input type="number" id="edad" name="edad">
+
+        
+        <br><br>
+
+        <label for="text">Correo electronico</label>
+        <input type="email" id="correo" name="correo">
+
+        
+        <br><br>
+
+        <label for="videojuego">VideoJuegos</label>
+        <select name="videojuego" id="videojuego">
+            <option value="">Selecione un Video Juego</option>
+            <option value="FiFa25">FiFA25</option>
+            <option value="Tekken8">Tekken8</option>
+            <option value="Valorant">Valorant</option>
+            <option value="Mario Kart 8">Mario Kart 8</option>
+
+        </select>
+
+         <br><br>
+
+        <label for="modalidad">Modalidad de participación:</label>
+        <select id="modalidad" name="modalidad">
+
+            <option value="">Seleccione una modalidad</option>
+            <option value="Individual">Individual</option>
+            <option value="Equipos">Equipos</option>
+
+        </select>
+
+        <br><br>
+
+        <label for="experiencia">Nivel de experiencia:</label>
+        <select id="experiencia" name="experiencia">
+
+            <option value="">Seleccione su experiencia</option>
+            <option value="Principiante">Principiante</option>
+            <option value="Intermedio">Intermedio</option>
+            <option value="Avanzado">Avanzado</option>
+
+        </select>
+
+        <br><br>
+
+        <button type="submit">enviar</button>
+
+
+
+    </form>
+</body>
+</html>
